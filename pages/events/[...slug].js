f6fbb9222ef9dfcaba06fbe6eaf37a6c72bf2dbe
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-10-05 15:46:25 +0300

    Feat(pages\events\[...slug].js): Extracting Data on the Catch-All Page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,44 @@
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
+
 function FilteredEventsPage() {
-    return <div>
-        <h1>Filtered Events</h1>
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p>Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    2021 > numYear > 2030 ||
+    1 > numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
+  return (
+    <div>
+      <h1 className="center">Filtered Events</h1>
     </div>
+  );
 }
 
-export default FilteredEventsPage
+export default FilteredEventsPage;
