9697f255998536d3ceb87c56d235e9e63503f097
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-10-26 17:41:14 +0300

    Adding Comment to the mongoDB

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -3,11 +3,11 @@ import { connectToDatabase } from "@/lib/mongodb";
 async function hanlder(req, res) {
     const evenId = req.query.eventId;
 
-    if(req.method === 'POST') {
-        const {email, name, text } = req.body;
+    if (req.method === 'POST') {
+        const { email, name, text } = req.body;
 
-        if(!email.includes('@') || !name || !text || text.trim() === '') {
-            res.status(422).json({message: 'Invalid input.'})
+        if (!email.includes('@') || !name || !text || text.trim() === '') {
+            res.status(422).json({ message: 'Invalid input.' })
             return;
         }
 
@@ -18,30 +18,30 @@ async function hanlder(req, res) {
             evenId
         }
 
-         try { 
-    const {db} = await connectToDatabase();
-    await db.collection('comments').insertOne(newComment);
-    res.status(201).json({message: 'Comment added successfully'})
-    } catch(err) {
-        res.status(501).json({message: "Not implemented"})
-    }
+        try {
+            const { db } = await connectToDatabase();
+            await db.collection('comments').insertOne(newComment);
+            res.status(201).json({ message: 'Comment added successfully' })
+        } catch (err) {
+            res.status(501).json({ message: "Not implemented" })
+        }
         console.log('New Comment: ', newComment);
-        res.status(201).json({message: 'Added comment'})
+        res.status(201).json({ message: 'Added comment' })
     }
 
-    if(req.method === 'GET') {
-    
-            try { 
-    const {db} = await connectToDatabase();
-    const documents = await db.collection('comments').find().sort({_id: -1}).toArray();
-    console.log(documents);
-   res.status(200).json({comments: documents});
-    } catch(err) {
-        res.status(501).json({message: "Not implemented"})
-    }
-        
+    if (req.method === 'GET') {
 
-        
+        try {
+            const { db } = await connectToDatabase();
+            const documents = await db.collection('comments').find().sort({ _id: -1 }).toArray();
+            console.log(documents);
+            res.status(200).json({ comments: documents });
+        } catch (err) {
+            res.status(501).json({ message: "Not implemented" })
+        }
+
+
+
     }
 }
 
