494053b850fc8390fef512e069213b42546c7196
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-10-26 19:38:22 +0300

    Using Context Data In Component

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,11 +1,19 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import MainHeader from "./main-header";
+import Notification from "../ui/notification";
+import NotificationContext from "@/store/notification-context";
 
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotifaciton = notificationCtx.notification;
+
+
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotifaciton && (<Notification title={activeNotifaciton.title} message={activeNotifaciton.message} status={activeNotifaciton.status} />)}
     </Fragment>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Layout from "@/components/layout/layout";
 import "@/styles/globals.css";
 import Head from "next/head";
-import Notification from "@/components/ui/notification";
 import { NotificationContextProvider } from "@/store/notification-context";
 
 export default function App({ Component, pageProps }) {
@@ -14,9 +13,7 @@ export default function App({ Component, pageProps }) {
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         </Head>
         <Component {...pageProps} />
-        <Notification title='Test' message='This is a test.' status='pending' />
       </Layout>
     </NotificationContextProvider>
-
   );
 }
